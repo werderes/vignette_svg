f85e8d3fed1c5bab765d1978924729f30ed58b0e
Pawel Smialek <werderes@gmail.com>
2023-01-28 17:04:57 +0100

    dupa

---
+++ main.py
@@ -0,0 +1,35 @@
+import svgwrite
+import math
+
+#vertices = 96
+#outside_ellipse_height = 120
+#outside_ellipse_width = 100
+#inside_ellipse_height = 90
+#inside_ellipse_width = 75
+
+with open('consts.txt') as f:
+    lines = f.readlines()
+    # A list containing the values of the constants
+    consts = [float(line) for line in lines if not line.startswith('#')]
+    vertices, outside_ellipse_height, outside_ellipse_width, inside_ellipse_height, inside_ellipse_width = consts
+
+vertices = int(vertices)
+pointes = []
+dwg = svgwrite.Drawing('star.svg', profile='tiny')
+
+for z in range(vertices):
+    t = z * 2 * math.pi / vertices
+    x1 = outside_ellipse_width * math.cos(t) / 2
+    y1 = outside_ellipse_height * math.sin(t) / 2
+    t += math.pi / vertices
+    x2 = inside_ellipse_width * math.cos(t) / 2
+    y2 = inside_ellipse_height * math.sin(t) / 2
+    t -= 2 * math.pi / vertices
+    #x3 = inside_ellipse_width * math.cos(t) / 2
+    #y3 = inside_ellipse_height * math.sin(t) / 2
+    pointes.append((x1*3.78, y1*3.78))
+    pointes.append((x2*3.78, y2*3.78))
+
+star = dwg.polygon(points=pointes, fill='none', stroke='black', stroke_width=0.1)
+dwg.add(star)
+dwg.save()
